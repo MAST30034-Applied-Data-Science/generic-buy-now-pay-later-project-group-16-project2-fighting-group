c84abd809d15ab1cabd644a05e71c257dcbad8de
SerenAdachi <adachis@student.unimelb.edu.au>
2022-10-16 23:03:54 +1100

    updated ETL script.py
    changed some comments
    and deleted unused codes

--- scripts/ETL script.py
+++ scripts/ETL script.py
@@ -25,7 +25,9 @@ spark = (
     .config("spark.sql.session.timeZone", "Etc/UTC")
     .getOrCreate()
 )
-## final transaction
+
+# ------------------------------------
+## OBTAINED THE PROCESSED TRANSACTION DATA
 # read the given transaction datasets
 ori_transaction1 = spark.read.parquet('data/tables/transactions_20210228_20210827_snapshot').sort('order_datetime')
 ori_transaction2 = spark.read.parquet('data/tables/transactions_20210828_20220227_snapshot').sort('order_datetime')
@@ -87,13 +89,14 @@ consumer_final.write.parquet(f"data/curated/final_consumer.parquet")
 transaction = ori_transaction.join(consumer_final, ori_transaction.user_id == consumer_final.user_id).drop(consumer_final.user_id)
 transaction = transaction.sort(transaction.user_id)
 transaction.write.parquet(f"data/curated/final_transaction.parquet")
-## final merchant
+
+# ------------------------------------
+## OBTAIN THE PROCESSED MERCHANT DATA
 # read the given merchant datasets
 merchant_fraud = spark.read.option("header",True).csv('data/tables/merchant_fraud_probability.csv')
 tbl_merchants = spark.read.parquet('data/tables/tbl_merchants.parquet')
 
 # check the range of date
-
 merchant_fraud = merchant_fraud.select(col("merchant_abn"),col("fraud_probability"),to_date(col("order_datetime"),"yyyy-MM-dd").alias("date"))
 
 # filter the range for the fraud data
@@ -126,8 +129,9 @@ for i in range(len(tbl_merchants_pd)):
 # export the merchant dataset
 merchants = tbl_merchants_pd[['merchant_abn', 'Store_type', 'Revenue_levels', 'Take_rate']]
 merchants.to_parquet(f"data/curated/final_merchant.parquet")
-## read external data
-# Go back one level from notebook to data 
+
+# ------------------------------------
+## RETRIEVE EXTERNAL DATA
 output_relative_dir = 'data/'
 
 if not os.path.exists(output_relative_dir):
@@ -140,9 +144,7 @@ for target_dir in ('external','outer'):
 # Download census data
 url = "https://www.abs.gov.au/census/find-census-data/datapacks/download/2021_GCP_SA2_for_AUS_short-header.zip"#year-month.parquet
 output_dir = "data/outer/census.zip"
-# download
 urlretrieve(url, output_dir) 
-
 files = zipfile.ZipFile('data/outer/census.zip','r')
 
 for file in files.namelist():
@@ -164,7 +166,6 @@ for type in types:
 # Download postcode and SA2 data
 url = "http://github.com/matthewproctor/australianpostcodes/zipball/master"
 output_dir = "data/outer/total_pto2.zip"
-# download
 urlretrieve(url, output_dir) 
 
 files = zipfile.ZipFile('data/outer/total_pto2.zip','r')
@@ -172,11 +173,8 @@ for file in files.namelist():
     files.extract(file, f"data/outer/total_pto2")
 
 post_sa2= spark.read.option("header",True).csv('data/outer/total_pto2/matthewproctor-australianpostcodes-6f8a994/australian_postcodes.csv') 
-print(post_sa2.dtypes)
 post_sa2 = post_sa2.select("id", "postcode", "Lat_precise", "Long_precise", "SA2_MAINCODE_2016")
-print(post_sa2.count() - post_sa2.na.drop().count())
 post_sa2 = post_sa2.na.drop()
-# 177 rows with nulls dropped
 
 # get the average latitude and longitude of the postcode if it have duplicates
 post_sa2 = post_sa2\
@@ -186,13 +184,10 @@ post_sa2 = post_sa2\
     .groupBy("postcode", "SA2_MAINCODE_2016")\
     .agg(F.avg("Lat_double").alias("avg_lat"), F.avg("Long_double").alias("avg_long"))
 
-#post_sa2.write.mode("overwrite").parquet("data/external/postcode_SA2.parquet")
-
 # Download SA2 2021 information
 url = "https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files/SA2_2021_AUST_SHP_GDA2020.zip" 
-# year-month.parquet
 output_dir = "data/outer/2021sa2_shapefile.zip"
-# download
+
 urlretrieve(url, output_dir) 
 print("complete")
 
@@ -216,39 +211,36 @@ table = DBF(path)
 sa2_pd_temp = pd.DataFrame(iter(table))
 sa2_2021_temp = spark.createDataFrame(sa2_pd_temp) 
 
+# extract only the data from Victoria state for visualisation 
 sa2_2021_vic = sa2_2021_temp.filter(F.col("STE_NAME21")=="Victoria")
 sa2_2021_vic_w_geo = sa2_2021_vic.withColumn("geometry", get_shapefile_udf(F.col("LOCI_URI21")))
 sa2_2021_vic_w_geo = sa2_2021_vic_w_geo.select("SA2_CODE21", "SA2_NAME21", "geometry")
-#sa2_2021_vic_w_geo.write.option("maxRecordsPerFile", 1).mode("overwrite").parquet("data/external/SA2_2021_VIC_shapefile.parquet")
 
 # Download file with 2016 SA2 info and 2021 SA2 info
 url = "https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/correspondences/CG_SA2_2016_SA2_2021.csv"
 output_dir = "data/outer/correspondences.csv"
-# download
 urlretrieve(url, output_dir) 
 
-correspondences= spark.read.option("header",True).csv('data/outer/correspondences.csv') #read the parquet 
+correspondences = spark.read.option("header",True).csv('data/outer/correspondences.csv') #read the parquet 
 correspondences = correspondences.na.drop()
-#correspondences.write.parquet("data/external/correspondences.parquet")
 
 # Find 2016 post code and 2021 sa2 correspondence
-print(post_sa2.dtypes)
-print(correspondences.dtypes)
 post_sa2_2021 = correspondences.join(post_sa2,correspondences.SA2_MAINCODE_2016 == post_sa2.SA2_MAINCODE_2016,"left") 
 
-# JOIN tables
+# JOIN tables: this will be the final SA2 and postcode correnpondence file
 post_sa2_2021 = post_sa2_2021.drop("SA2_NAME_2016", "SA2_MAINCODE_2016")
 post_sa2_2021.write.parquet("data/external/postcode_sa2_conrrespondences.parquet")
-## census_age&income
+
+
+# ------------------------------------
+## PROCESS CENSUS AGE AND INCOME DATA
 # read the external census datasets
 census_04A_age = spark.read.parquet('data/external/census_data_2021_04A.parquet')
 census_04B_age = spark.read.parquet('data/external/census_data_2021_04B.parquet')
 census_17A_income = spark.read.parquet('data/external/census_data_2021_17A.parquet')
 census_17B_income = spark.read.parquet('data/external/census_data_2021_17B.parquet')
 census_17C_income = spark.read.parquet('data/external/census_data_2021_17C.parquet')
-#correspondences = spark.read.parquet('data/external/correspondences.parquet')
 postcode = post_sa2
-#postcode = spark.read.parquet('data/external/postcode_SA2.parquet/')
 
 # Merge the age dataframe
 Age_Data = census_04A_age.join(census_04B_age, census_04A_age.SA2_CODE_2021 == census_04B_age.SA2_CODE_2021).drop(census_04B_age.SA2_CODE_2021)
@@ -257,16 +249,20 @@ Age_Data = census_04A_age.join(census_04B_age, census_04A_age.SA2_CODE_2021 == c
 Age_Data_select = Age_Data.select(Age_Data.columns[0::18])
 Age_Data_total = Age_Data_select.withColumn('result',reduce(add, [F.col(x) for x in Age_Data_select.columns[5:14]]))
 Age_Data_total = Age_Data_total.select("SA2_CODE_2021","result","Tot_P")
+
 # Calculate the percentage of the targeted age group of all people
 Age_Data_rate = Age_Data_total.withColumn("age_percentage", Age_Data_total.result /Age_Data_total.Tot_P)
 Age_Data_rate = Age_Data_rate.select("SA2_CODE_2021","age_percentage")
+
 # Combine income table
 income = census_17A_income.join(census_17B_income, census_17A_income.SA2_CODE_2021 == census_17B_income.SA2_CODE_2021).drop(census_17B_income.SA2_CODE_2021)
 income_data = income.join(census_17C_income, income.SA2_CODE_2021 == census_17C_income.SA2_CODE_2021).drop(census_17C_income.SA2_CODE_2021)
+
 # Select the column in the income that above Australia medium income and calculate the sum
 income_data_del = income_data.select(income_data.columns[0::10])
 income_data_select = income_data_del.withColumn('result',reduce(add, [F.col(x) for x in income_data_del.columns[44:50]]))
 income_Data_total = income_data_select.select("SA2_CODE_2021","result","P_Tot_Tot")
+
 # Calculate the percentage of the targeted income group of all people
 income_Data_total = income_Data_total.withColumn("income_percentage", income_Data_total.result / income_Data_total.P_Tot_Tot)
 income_Data_total = income_Data_total.select("SA2_CODE_2021","income_percentage")
